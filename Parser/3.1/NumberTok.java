b1dd3988ba469f54c8111b7b00f950a7d18b81b8
SteuFonta01 <stefano.fontana2001@gmail.com>
2022-01-11 19:09:56 +0100

    Version 0.9 [11/01/2022]: Loaded Parser 3.2; refactoring, bugfixing and testing in Parser implementation

--- Parser/3.1/NumberTok.java
+++ Parser/3.1/NumberTok.java
@@ -1,26 +1,20 @@
 public class NumberTok extends Token {
-	// questa classe implementa la parte del lessema
-	// per i token corrispondenti a costanti numeriche
 	/*
-	 * This class implements the lexeme field of numeric constants' tokens.
-	 */
-	public int lexeme = 0;
+		This class implements the lexeme field of numeric
+		constants' tokens. 
+	*/
+	public int lexem = 0;
 
-	public NumberTok(int tag, int lexeme) {
-		super(tag); // richiama il costruttore di Token
+	public NumberTok(int tag, int lexem) {
+		super(tag);
 
-		if (lexeme > -1)
-			this.lexeme = lexeme;
+		if (lexem > -1)
+			this.lexem = lexem;
 		else
 			System.out.println("Invalid numeric lexem");
 	}
 
-	public String toString() {
-		return "<" + tag + ", " + lexeme + ">";
+	public String toString(){
+		return "<" + tag + ", " + lexem + ">";
 	}
-
-	// qui non conosciamo in anticipo i lessemi che formeranno
-	// i vari token, quindi non possiamo definirli anticipatamente
-	// Li definiremo durante la scansione, quando sarà noto il valore
-	// della costante numerica rappresentata dal token
 }
--- Parser/3.1/Tag.java
+++ Parser/3.1/Tag.java
@@ -1,7 +1,20 @@
 public class Tag {
-    public final static int
-	EOF = -1, NUM = 256, ID = 257, RELOP = 258,
-	ASSIGN = 259, TO = 260, IF = 261, ELSE = 262, 
-	WHILE = 263, BEGIN = 264, END = 265, PRINT = 266, READ = 267, 
-	OR = 268, AND = 269;
+	// List of possible tag values
+	// for the language's tokens
+	public final static int EOF = -1, 
+							NUM = 256, 
+							ID = 257, 
+							RELOP = 258, 
+							ASSIGN = 259, 
+							TO = 260, 
+							IF = 261, 
+							ELSE = 262,
+							WHILE = 263, 
+							BEGIN = 264, 
+							END = 265, 
+							PRINT = 266, 
+							READ = 267, 
+							OR = 268, 
+							AND = 269,
+							COMMENT = 270;
 }
