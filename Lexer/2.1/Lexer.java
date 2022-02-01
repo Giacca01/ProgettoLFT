c7d255c34e7c163d0c3f0e0a2717fcc72a52df60
Giacca01 <fgiacardi01@gmail.com>
2022-02-01 10:42:14 +0100

    Version 1.2[01/02/2022]: lexer splitted into three different exercises

---
+++ Lexer/2.1/Lexer.java
@@ -0,0 +1,309 @@
+import java.io.*;
+import java.util.*;
+
+public class Lexer {
+
+    enum IdentifiersStates {
+        Q0, Q1, Q2, INVALID
+    };
+
+    public static int line = 1;
+    private char peek = ' ';
+    // HashMap that allows to retrive key words' token
+    // in constant time ( O(1) )
+    private static HashMap<String, Integer> keyWords = new HashMap<>();
+
+    /**
+     * Constructor of the Lexer class, initializes the keyWords HashMap
+     */
+    public Lexer() {
+        // Initializing the keyword's map
+        keyWords.put("assign", Tag.ASSIGN);
+        keyWords.put("to", Tag.TO);
+        keyWords.put("if", Tag.IF);
+        keyWords.put("else", Tag.ELSE);
+        keyWords.put("while", Tag.WHILE);
+        keyWords.put("begin", Tag.BEGIN);
+        keyWords.put("end", Tag.END);
+        keyWords.put("print", Tag.PRINT);
+        keyWords.put("read", Tag.READ);
+    }
+
+    /**
+     * Function that reads and returns a character from the given stream
+     * 
+     * @param br: stream to read characters from
+     * @return character read
+     */
+    private void readch(BufferedReader br) {
+        try {
+            peek = (char) br.read();
+        } catch (IOException exc) {
+            peek = (char) -1; // ERROR
+        }
+    }
+
+    /**
+     * Scans the input stream to return the next token
+     * 
+     * @param br: stream whose characters are to be tokenized
+     * @return The next valid token or a null pointer
+     */
+    public Token lexical_scan(BufferedReader br) {
+        Token returnValue = null;
+        String aus = "";
+        IdentifiersStates idState = IdentifiersStates.Q0;
+        int i = 0;
+        Character symbol = ' ';
+
+        // formatting, meaningless symbols are discarded
+        while (peek == ' ' || peek == '\t' || peek == '\n' || peek == '\r') {
+            if (peek == '\n') {
+                System.out.println("***************************");
+                line++;
+            }
+            // reads next character from input buffer
+            readch(br);
+        }
+
+        // analyzes the symbol to create corresponding token
+        switch (peek) {
+            case '!':
+                // we use ' ' as a way to request
+                // the reading of the next character
+                peek = ' ';
+                returnValue = Token.not;
+                break;
+
+            case '(':
+                // Since we're not performing a syntactical
+                // analysis we do not need to know the following
+                // chacter to recognize the '(' symbol
+                peek = ' ';
+                returnValue = Token.lpt;
+                break;
+
+            case ')':
+                peek = ' ';
+                returnValue = Token.rpt;
+                break;
+
+            case '+':
+                peek = ' ';
+                returnValue = Token.plus;
+                break;
+
+            case '-':
+                peek = ' ';
+                returnValue = Token.minus;
+                break;
+
+            case '*':
+                peek = ' ';
+                returnValue = Token.mult;
+                break;
+
+            case '/':
+                peek = ' ';
+                returnValue = Token.div;
+                break;
+
+            case ';':
+                peek = ' ';
+                returnValue = Token.semicolon;
+                break;
+
+            case ',':
+                peek = ' ';
+                returnValue = Token.comma;
+                break;
+
+            case '{':
+                peek = ' ';
+                returnValue = Token.lpg;
+                break;
+
+            case '}':
+                peek = ' ';
+                returnValue = Token.rpg;
+                break;
+
+            case '&':
+                // we read the next character
+                readch(br);
+                // and check if it's another &
+                if (peek == '&') {
+                    // if it's the case, we have found
+                    // an and token and we output it
+                    peek = ' ';
+                    returnValue = Word.and;
+                } else
+                    System.err.println("Erroneous character" + " after & : " + peek);
+
+                break;
+
+            case '|':
+                readch(br);
+
+                if (peek == '|') {
+                    peek = ' ';
+                    returnValue = Word.or;
+                } else
+                    System.err.println("Erroneous character" + " after | : " + peek);
+
+                break;
+
+            case '<':
+                readch(br);
+
+                if (peek == '='){
+                    peek = ' ';
+                    returnValue = Word.le;
+                } else if (peek == '>'){
+                    peek = ' ';
+                    returnValue = Word.ne;
+                } else {
+                    /*
+                        We don't want to read another character, because by doing
+                        that we would discard the character that follows <, that wasn't
+                        used to produce the Word.lt token, and can therefore match the patter
+                        of a different token
+                    */
+                    returnValue = Word.lt;
+                }
+
+                break;
+
+            case '>':
+                readch(br);
+
+                if (peek == '='){
+                    peek = ' ';
+                    returnValue = Word.ge;
+                } else {
+                    returnValue = Word.gt;
+                }
+
+                break;
+
+            case '=':
+                readch(br);
+
+                if (peek == '=') {
+                    peek = ' ';
+                    returnValue = Word.eq;
+                } else {
+                    System.err.println("Erroneous character" + " after = : " + peek);
+                    returnValue = null;
+                }
+
+                break;
+
+            case (char) -1:
+                returnValue = new Token(Tag.EOF);
+                break;
+
+            default:
+                if (Character.isLetter(peek)) { // neither keyword nor identifiers can start with numbers
+
+                    aus = Character.toString(peek);
+                    readch(br);
+
+                    while (Character.isLetterOrDigit(peek)) {
+                        aus += Character.toString(peek);
+                        readch(br);
+                    }
+
+                    if (keyWords.containsKey(aus)) { // keyword
+                        returnValue = new Word(keyWords.get(aus), aus);
+                    } else { // identifier
+                        returnValue = new Word(Tag.ID, aus);
+                    }
+
+                } else if (Character.isDigit(peek)) {
+                    if (peek == '0') {
+                        peek = ' ';
+                        returnValue = new NumberTok(Tag.NUM, 0);
+                    } else {
+                        aus = "";
+                        do { // same logic used for words
+                            aus += peek;
+                            readch(br);
+                        } while (Character.isDigit(peek)); // we read all the number's digits to form the lexem
+
+                        returnValue = new NumberTok(Tag.NUM, Integer.parseInt(aus));
+                    }
+
+                } else
+                    System.err.println("Erroneous character: " + peek);
+
+                break;
+        }
+
+        return returnValue;
+    }
+
+    public static boolean isID(String aus) { // check if s is ID
+        IdentifiersStates idState = IdentifiersStates.Q0;
+        int i = 0;
+        Character symbol = ' ';
+        // check identifier's validity
+        while (i < aus.length() && idState != IdentifiersStates.INVALID) {
+            symbol = aus.charAt(i);
+            switch (idState) {
+                case Q0:
+                    if (Character.isLetter(symbol))
+                        idState = IdentifiersStates.Q2;
+                    else if (symbol == '_')
+                        idState = IdentifiersStates.Q1;
+                    else
+                        idState = IdentifiersStates.INVALID;
+                    break;
+
+                case Q1:
+                    if (Character.isLetterOrDigit(symbol)) {
+                        idState = IdentifiersStates.Q2;
+                    } else if (symbol == '_')
+                        idState = IdentifiersStates.Q1;
+                    else
+                        idState = IdentifiersStates.INVALID;
+                    break;
+
+                case Q2:
+                    if (!Character.isLetterOrDigit(symbol) && symbol != '_')
+                        idState = IdentifiersStates.INVALID;
+                    break;
+
+                default:
+                    idState = IdentifiersStates.INVALID;
+                    break;
+            }
+            i++;
+        }
+
+        return idState == IdentifiersStates.Q2;
+    }
+
+    public static void main(String[] args) {
+        Lexer lex = new Lexer();
+        String path = "Prova.txt"; // il percorso del file da leggere
+
+        // Open an input stream and prints the lexer's output
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            Token tok;
+
+            do {
+                tok = lex.lexical_scan(br);
+                if (tok != null)
+                    System.out.println("Scan: " + tok);
+
+            } while (tok != null && tok.tag != Tag.EOF);
+
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
